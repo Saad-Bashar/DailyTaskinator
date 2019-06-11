db749b073c2fa09a3e99871b1802fbac52fb61cd
Saad <saad@socar.my>
2019-06-11 18:10:39 +0800

    fix release build for android

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -1,28 +1,7 @@
 // leave off @2x/@3x
 const images = {
   logo: require('../Images/logo.jpg'),
-  logoNew: require('../Images/logo.png'),
-  clearLogo: require('../Images/top_logo.png'),
-  launch: require('../Images/launch-icon.png'),
-  ready: require('../Images/your-app.png'),
-  ignite: require('../Images/ignite_logo.png'),
-  igniteClear: require('../Images/ignite-logo-transparent.png'),
-  tileBg: require('../Images/tile_bg.png'),
-  background: require('../Images/BG.png'),
-  buttonBackground: require('../Images/button-bg.png'),
-  api: require('../Images/Icons/icon-api-testing.png'),
-  components: require('../Images/Icons/icon-components.png'),
-  deviceInfo: require('../Images/Icons/icon-device-information.png'),
-  faq: require('../Images/Icons/faq-icon.png'),
-  home: require('../Images/Icons/icon-home.png'),
-  theme: require('../Images/Icons/icon-theme.png'),
-  usageExamples: require('../Images/Icons/icon-usage-examples.png'),
-  chevronRight: require('../Images/Icons/chevron-right.png'),
-  hamburger: require('../Images/Icons/hamburger.png'),
-  backButton: require('../Images/Icons/back-button.png'),
-  closeButton: require('../Images/Icons/close-button.png'),
   graphics: require('../Images/graphics.png'),
-  // graphicsCenter: require('../Images/graphics-center.png'),
 };
 
 export default images;
