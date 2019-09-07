937ad5cd44bd37de0d2411d263554982810cbef3
Saad <saad@socar.my>
2019-09-07 23:01:56 +0800

    empty state added

--- App/Themes/Images.js
+++ App/Themes/Images.js
@@ -2,6 +2,10 @@
 const images = {
   logo: require('../Images/logo.png'),
   graphics: require('../Images/graphics.png'),
+  notFound: require('../Images/not-found.png'),
+  notFoundFamily: require('../Images/not-found-family.png'),
+  notFoundWork: require('../Images/not-found-work.png'),
+  notFoundPersonal: require('../Images/not-found-personal.png'),
 };
 
 export default images;
